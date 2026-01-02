e96440ed5016e9e2b8105d778b90bbbf69456e66
AayushMitra-glitch <aayushmitra6@gmail.com>
2026-01-02 12:13:22 +0530

    Update Aim-2.C

---
+++ Practical-3/Aim-2.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// Aayush Mitra 10025
+int main() {
+float a = 12.6;
+float b = 4.2;
+float c;
+c = a - b;
+printf("DIFFERENCE of two numbers: %f", c);
+return 0;
+}
+// DIFFERENCE of two numbers: 8.400001
+
+// === Code Execution Successful ===
