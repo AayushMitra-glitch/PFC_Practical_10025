a7a8f6d7614679c473a7f901b8da47a2b05b8ff5
AayushMitra-glitch <aayushmitra6@gmail.com>
2026-01-02 12:15:54 +0530

    Update Run_time_error.C

---
+++ Practical-4/Run_time_error.C
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+//  Aayush Mitra 10025
+int main(){
+int numerator = 10;
+int denominator = 0;
+int result = numerator / denominator; // Division by zero
+printf("Result: %d\n", result);
+return 0;
+}
+
+// Floating point exception
+
+
+// === Code Exited With Errors ===
