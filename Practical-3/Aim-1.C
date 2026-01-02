6ee62c94ad7c353c762cc1ae30236b25e5703307
AayushMitra-glitch <aayushmitra6@gmail.com>
2026-01-02 12:12:52 +0530

    Update Aim-1.C

---
+++ Practical-3/Aim-1.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // Aayush Mitra 10025
+    int a = 10;
+    int b = 15;
+    int c = a + b;
+    printf("Aayush Mitra\n");
+    printf("Sum of two numbers: %d", c);
+    return 0;
+}
+
+// Aayush Mitra
+// Sum of two numbers: 25
+
+// === Code Execution Successful ===
