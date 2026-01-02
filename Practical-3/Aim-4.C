0df7505de923d0185b33a6a8c508bfd6078a6e7c
AayushMitra-glitch <aayushmitra6@gmail.com>
2026-01-02 12:13:54 +0530

    Update Aim-4.C

---
+++ Practical-3/Aim-4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// Aayush Mitra 10025
+int main() {
+    char a = 'C';
+    printf("Character is %c\n", a);
+    printf("ASCII code of character is %d\n", a);
+    char b = a + 3;
+    printf("Three characters after is %c\n", b);
+    return 0;
+}
+
+// Character is C
+// ASCII code of character is 67
+// Three characters after is F
+
+
+// === Code Execution Successful ===
