59de9ae296207ccb6985c34d00f5dda9061453d6
AayushMitra-glitch <aayushmitra6@gmail.com>
2026-01-02 12:22:09 +0530

    Update Aim-2.C

---
+++ Practical-8/Aim-2.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// Aayush Mitra 10025
+
+int sumOfDigits(int n) {
+    int sum = 0;
+    while(n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    printf("Sum of digits = %d", sumOfDigits(n));
+    return 0;
+}
+//OUTPUT
+// Enter number: 32
+// Sum of digits = 5
