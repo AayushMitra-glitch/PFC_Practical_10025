990e35dddac79681a352df855816a4be4bb4fa26
AayushMitra-glitch <aayushmitra6@gmail.com>
2026-01-02 12:10:22 +0530

    Update Aim-1.C

---
+++ Practical-1/Aim-1.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+// Aayush Mitra 10025
+printf("Aayush Mitra\n");
+printf("ERP-RU-25-10025\n");
+
+printf("B.Tech 1st Semester CSE (AI & ML) Affiliated by Google\n");
+printf("This is my first C program.\n");
+return 0;
+}
+//Aayush Mitra
+// ERP-RU-25-10025
+// B.Tech 1st Semester CSE (AI & ML) Affiliated by Google
+// This is my first C program.
+
+
+            // === Code Execution Successful ===
